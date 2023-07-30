0a6572c17371364f829afc4ecb6ee3ac3e09c7f1
熊谷柊弥 <ksuimrao.studio@gmail.com>
2023-07-30 10:31:48 +0900

    Added the ability to open a new tab and paste the value of the default register.

--- lua/my_plugin.lua
+++ lua/my_plugin.lua
@@ -1,6 +1,10 @@
 local my_plugin = {}
 -- my_pluginテーブル（配列）に、helloという関数を詰める
 function my_plugin.hello()
+    vim.api.nvim_command('tabnew')
+
+    local register_content = vim.fn.getreg('')
+    vim.api.nvim_put({register_content}, 'l', true, true)
 end
 
 return my_plugin
